7b90a12905de62345e43bbf15b007ab330ac8fb3
lean456 <leandroblanco.456@gmail.com>
2022-06-20 17:40:44 -0300

    Commit de observer y subscribe

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
+import { Observable, Observer } from "rxjs";
 
+const observer: Observer<any> = {
+  next: (value) => console.log("siguiente [next]:", value),
+  error: (error) => console.warn("error [obs]:", error),
+  complete: () => console.info("completado [obs]"),
+};
+const obs$ = new Observable<string>((subs) => {
+  subs.next("hola");
+  subs.next("mundo");
 
-console.log('Hola Mundo!');
+  subs.next("hola");
+  subs.next("mundo");
 
+  //   const a = undefined;
+  //   a.nombre = "Leandro";
+  subs.complete();
 
+  subs.next("hola");
+  subs.next("mundo");
+});
 
-
-
-
-
+obs$.subscribe(observer);
+// obs$.subscribe(
+//   (valor) => console.log("next:", valor),
+//   (error) => console.warn("error", error),
+//   () => console.info("complete")
+// );
